4f63c2eaa06173bf999e475dd0a851252254aa51
EuSolluna <sol.luna@escolar.ifrn.edu.br>
2023-12-07 17:45:51 -0300

    salve

--- boletim/app.py
+++ boletim/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, url_for, session, request, jsonify, render_template
+from flask import Flask, redirect, url_for, session, request, render_template
 from authlib.integrations.flask_client import OAuth
-import config 
+import requests
 
 app = Flask(__name__)
 app.debug = True
@@ -9,8 +9,8 @@ oauth = OAuth(app)
 
 oauth.register(
     name='suap',
-    client_id= config.client_id,
-    client_secret= config.client_secret
+    client_id= "tJIwB2VZ5WUmVThDMIisVsUYXrYlJ4Tz5JzEcy6y",
+    client_secret= "2vw9Gw8sGge7qChFx2SjGsQBxA24D6v2IckpGkI0Yo2ivIdpI1LJgBxTmzmo0sDDZES1n0oDdJ4PLVdD1WIlPZ3nGgAfO7LHiqadFIWBVJblQF8BWKtDJ4j1cMlQYV9u",
     api_base_url='https://suap.ifrn.edu.br/api/',
     request_token_url=None,
     access_token_method='POST',
@@ -19,6 +19,13 @@ oauth.register(
     fetch_token=lambda: session.get('suap_token')
 )
 
+def get_boletim(ano, bimestre):
+    if ano and bimestre:
+        token = session.get('suap_token')['access_token']
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(f'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/boletim/{ano}/{bimestre}/', headers=headers)
+        return response.json()
+    return None
 
 @app.route('/')
 def index():
@@ -28,22 +35,41 @@ def index():
     else:
         return render_template('index.html')
 
-
 @app.route('/login')
 def login():
     redirect_uri = url_for('auth', _external=True)
-    print(redirect_uri)
     return oauth.suap.authorize_redirect(redirect_uri)
-
 
 @app.route('/logout')
 def logout():
     session.pop('suap_token', None)
     return redirect(url_for('index'))
 
-
 @app.route('/login/authorized')
 def auth():
     token = oauth.suap.authorize_access_token()
     session['suap_token'] = token
     return redirect(url_for('index'))
+
+# Função principal para a rota '/user'
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if 'suap_token' not in session:
+        return redirect(url_for('index'))
+
+    boletim_data = None
+    ano = None
+    bimestre = None
+
+    if request.method == 'POST':
+        ano = request.form.get('ano')
+        bimestre = request.form.get('periodo')
+
+        # Chamada da função para obter o boletim
+        boletim_data = get_boletim(ano, bimestre)
+
+    meus_dados = oauth.suap.get('v2/minhas-informacoes/meus-dados')
+    return render_template('user.html', user_data=meus_dados.json(), boletim=boletim_data, ano=ano, bimestre=bimestre)
+
+if __name__ == '__main__':
+    app.run()
